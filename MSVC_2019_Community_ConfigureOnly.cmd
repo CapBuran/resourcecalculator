4a0ea07fd00212193b78bf2db04dac5f71d2d898
Vasiliy Kozyrev <ezqc123@gmail.com>
2021-01-07 19:24:38 +0300

    Build on windows

---
+++ MSVC_2019_Community_ConfigureOnly.cmd
@@ -0,0 +1,26 @@
+SET REP=%CD%
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+SET QT_VERSION=5.15.2
+SET QT_INSTALL_DIR=C:\QT
+SET QT_VER_DIR=msvc2019_64
+
+CALL %QT_INSTALL_DIR%\%QT_VERSION%\%QT_VER_DIR%\bin\qtenv2.bat
+
+PUSHD ..
+SET MyRepository=%CD%
+PUSHD ..
+SET MyProjectDir=%CD%\%compiler%_%versionVS%
+SET MyDeployDir=%CD%\%compiler%_%versionVS%_QT_DEPLOY
+
+set CMAKE_EXE=cmake.exe
+if exist "C:\Program Files (x86)\CMake\bin\cmake.exe" set CMAKE_EXE="C:\Program Files (x86)\CMake\bin\cmake.exe"
+if exist "C:\Program Files\CMake\bin\cmake.exe" set CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
+
+if not exist "%REP%\bin" MKDIR "%REP%\bin"
+
+PUSHD "%REP%\bin"
+%CMAKE_EXE% -A x64 "%REP%" > "%REP%\cmake.log" 2>&1
+::%CMAKE_EXE% --build . --config debug  > "%REP%\build_debug.log" 2>&1
+::%CMAKE_EXE% --build . --config release  > "%REP%\build_release.log" 2>&1
+POPD
